eca817e35554ed2a560f1b8e2976780c2dd0951a
Aditi Singh <787aditi@gmail.com>
2016-10-12 17:37:32 -0500

    Added labels

--- plot_pr_curve.m
+++ plot_pr_curve.m
@@ -5,6 +5,8 @@ load 'oversegment/pr_vals.mat';
 pr1=all_pr(1:end,:);
 pr2=all_pr(2:end,:);
 plot(pr2(:,2),pr2(:,1),'+','MarkerSize',12, 'LineWidth',2);
+labels={'NA';'DAPI';'S100';'NeuN';'TubulinBeta3';'GFAP';'NA';'NFH';'NA';'SynaptoPhys';'GLAST';'TomatoLectin'};
+text(all_pr(:,2),all_pr(:,1),labels,'VerticalAlignment','bottom','HorizontalAlignment','right');
 set(gca,'FontSize',14,'FontWeight','bold');
 h = xlabel('Recall');
 set(h,'FontSize',14,'FontWeight','bold');
